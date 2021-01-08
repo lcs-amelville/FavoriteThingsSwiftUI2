cca934eab663290cc9de7fdf0fbc5bf956dfb28e
Aidan Melville <amelville@lcs.on.ca>
2021-01-08 14:30:31 -0500

    Initial Commit

---
+++ FavoriteThingsSwiftUI2/FavoriteThingsSwiftUI2App.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteThingsSwiftUI2App.swift
+//  FavoriteThingsSwiftUI2
+//
+//  Created by Melville, Aidan on 2021-01-08.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteThingsSwiftUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
